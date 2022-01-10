ba6387bffc58b2614e052804319885d8a5d430c8
Karim KASSAB <karimkassabb@gmail.com>
2022-01-10 18:05:52 +0100

    pep8-3

--- numpy_questions.py
+++ numpy_questions.py
@@ -65,7 +65,6 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-
     pi = 1
 
     for i in range(1, n_terms+1):
