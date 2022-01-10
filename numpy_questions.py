2d26219afc8f91e06c5c2453bbf879124c031853
Antoine Palazzolo <antoine.palazzolo@kes.eleves.polytechnique.fr>
2022-01-10 16:28:37 +0100

    Update numpy_questions.py

--- numpy_questions.py
+++ numpy_questions.py
@@ -37,7 +37,6 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
-
     if (not isinstance(X, np.ndarray)) or (len(X.shape) != 2):
         raise ValueError
 
@@ -65,7 +64,6 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
 
