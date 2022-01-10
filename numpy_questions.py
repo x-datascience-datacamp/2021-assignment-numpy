6c84c461d405a1d1f1a88f4031755245c722e4df
BenJ-cell <67470526+BenJ-cell@users.noreply.github.com>
2022-01-10 18:23:17 +0100

    Update numpy_questions.py

--- numpy_questions.py
+++ numpy_questions.py
@@ -1,36 +1,29 @@
 """Assignment - using numpy and making a PR.
-
 The goals of this assignment are:
     * Use numpy in practice with two easy exercises.
     * Use automated tools to validate the code (`pytest` and `flake8`)
     * Submit a Pull-Request on github to practice `git`.
-
 The two functions below are skeleton functions. The docstrings explain what
 are the inputs, the outputs and the expected error. Fill the function to
 complete the assignment. The code should be able to pass the test that we
 wrote. To run the tests, use `pytest test_numpy_question.py` at the root of
 the repo. It should say that 2 tests ran with success.
-
 We also ask to respect the pep8 convention: https://pep8.org.
 This will be enforced with `flake8`. You can check that there is no flake8
 errors by calling `flake8` at the root of the repo.
 """
 import numpy as np
 
-
 def max_index(X):
     """Return the index of the maximum in a numpy array.
-
     Parameters
     ----------
     X : ndarray of shape (n_samples, n_features)
         The input array.
-
     Returns
     -------
     (i, j) : tuple(int)
         The row and columnd index of the maximum.
-
     Raises
     ------
     ValueError
@@ -39,29 +32,45 @@ def max_index(X):
     """
     i = 0
     j = 0
+    max = 0
 
     # TODO
-
+    # Get the indices of one the maximum element in numpy array
+    if len(np.shape(X)) != 2:
+        raise ValueError
+    n_samples,n_features = np.shape(X)
+    for k in range(n_samples):
+      for q in range(n_features):
+        if (X[k][q]>max):
+          max = X[k][q]
+          i,j = k,q
+        else :
+          None
     return i, j
 
 
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
-
     See:
     https://en.wikipedia.org/wiki/Wallis_product
-
     Parameters
     ----------
     n_terms : int
         Number of steps in the Wallis product. Note that `n_terms=0` will
         consider the product to be `1`.
-
     Returns
     -------
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
     # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    return 0.
+    # terms in the pr
+    if n_terms < 0:
+        raise ValueError("Number of terms negative")
+    if (n_terms==0):
+      return 2 
+    elif (type(n_terms)==int) :
+      A = 1
+      for i in range(1,n_terms+1):
+        A = A*((4*(i**2))/(4*(i**2) - 1))
+      return 2*A
