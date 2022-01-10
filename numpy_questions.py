6db587d2543880715379a6bd758448389cd79da9
walid-yassine <walidyassine98@gmail.com>
2022-01-10 15:44:48 +0100

    WY tentative solution

--- numpy_questions.py
+++ numpy_questions.py
@@ -37,11 +37,17 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
+
+    if(type(X).__module__ != np.__name__):
+        raise ValueError
+    if(len(X.shape) != 2):
+        raise ValueError
+
     i = 0
     j = 0
 
     # TODO
-    i, j =np.argmax(X)
+    i, j = np.unravel_index(np.argmax(X), X.shape)
 
     return i, j
 
@@ -65,4 +71,8 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+
+    pi = 1.
+    for i in range(1, n_terms + 1):
+        pi *= (4*(i**2))/((4*(i**2)) - 1)
+    return 2*pi
