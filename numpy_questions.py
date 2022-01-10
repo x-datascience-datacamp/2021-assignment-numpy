6caaed35979df1a4144354f17c645e9d5af9b994
Paul Orluc <paulorluc@yahoo.fr>
2022-01-10 16:44:31 +0100

    UP my solution

--- numpy_questions.py
+++ numpy_questions.py
@@ -41,11 +41,28 @@ def max_index(X):
     j = 0
 
     # TODO
+    if type(X)!=np.ndarray:
+        raise ValueError
+    if len(X.shape)!=2:
+        raise ValueError
+    s=X[0][0]
+    ii=0
+    while ii<len(X):
+        jj=0
+        while jj<len(X[0]):
+            if X[ii][jj]>X[i][j]:
+                j=jj
+                i=ii
+            jj=jj+1
+        ii=ii+1
+
 
     return i, j
 
 
 def wallis_product(n_terms):
+
+
     """Implement the Wallis product to compute an approximation of pi.
 
     See:
@@ -64,4 +81,19 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    if type(n_terms)!=int:
+        raise ValueError
+
+    if n_terms<0:
+        raise ValueError
+    if n_terms==0:
+        return(2)
+
+    i=1
+    p=1
+    while i<n_terms+1:
+        p=p*(4*i**2)/(4*i**2-1)
+        i=i+1
+    return(2*p)
+
+
