fc28328330633c9346a540276603062791ec9da9
egrillot <44230801+egrillot@users.noreply.github.com>
2022-01-10 16:01:06 +0100

    Update numpy_questions.py

--- numpy_questions.py
+++ numpy_questions.py
@@ -20,48 +20,63 @@ import numpy as np
 
 def max_index(X):
     """Return the index of the maximum in a numpy array.
-
     Parameters
     ----------
     X : ndarray of shape (n_samples, n_features)
         The input array.
-
     Returns
     -------
     (i, j) : tuple(int)
         The row and columnd index of the maximum.
-
     Raises
     ------
     ValueError
         If the input is not a numpy array or
         if the shape is not 2D.
     """
+
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        raise ValueError('Please the input must be an array in 2D')
+
     i = 0
     j = 0
 
-    # TODO
+    max_array = X[i, j]
+    ind = 0
+
+    while ind < X.shape[0]:
+        m = np.max(X[ind, :])
+        if m > max_array:
+            max_array = m
+            i = ind 
+            j = np.argmax(X[ind, :])
+        ind += 1
 
     return i, j
 
 
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
-
     See:
     https://en.wikipedia.org/wiki/Wallis_product
-
     Parameters
     ----------
     n_terms : int
         Number of steps in the Wallis product. Note that `n_terms=0` will
         consider the product to be `1`.
-
     Returns
     -------
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    return 0.
+    if n_terms == 0:
+        return 2
+
+    wallis_term = 4/3
+    for i in range(2, n_terms + 1):
+
+        u = 2*i / (2*i - 1)
+        v = 2*i / (2*i + 1)
+        wallis_term *= u * v
+
+    return wallis_term * 2
