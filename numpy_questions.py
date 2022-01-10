f479e79cf8deb02426f55fcc7904c3b2962a507b
Louis Popi <theh2o64@gmail.com>
2022-01-10 18:14:46 +0100

    2021-assignment-numpy: Fix pydocstyle error D202

--- numpy_questions.py
+++ numpy_questions.py
@@ -37,7 +37,6 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
-
     # Check if it's a numpy array
     if not isinstance(X, np.ndarray):
         raise ValueError
