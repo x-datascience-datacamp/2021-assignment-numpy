3c6a3cc4731688ad8ce19af5b823d784c45a8388
GaspardAndre <97414528+GaspardAndre@users.noreply.github.com>
2022-01-10 16:54:57 +0100

    iter

--- numpy_questions.py
+++ numpy_questions.py
@@ -41,6 +41,12 @@ def max_index(X):
     j = 0
 
     # TODO
+    if(type(X)!=np.ndarray):
+        raise ValueError('X is not a numpy array')
+    elif(len(X.shape)!=2 ):
+        raise ValueError('The shape is not 2D')
+    else:
+        i,j = np.unravel_index(np.argmax(X, axis=None), X.shape)
 
     return i, j
 
@@ -64,4 +70,11 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    if n_terms==0:
+        return 2
+    
+    N = np.arange(1,n_terms+1)
+    q = (4*N**2)/(4*N**2-1)
+    p = np.cumprod(q)[-1]
+    
+    return 2*p
