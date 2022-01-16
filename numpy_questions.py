e891fd400124a0b303e1ad2c033bb3940d62546b
jujujujukim <juhyun.kim@ip-paris.fr>
2022-01-16 16:10:26 +0100

    Solution

--- numpy_questions.py
+++ numpy_questions.py
@@ -16,7 +16,6 @@ This will be enforced with `flake8`. You can check that there is no flake8
 errors by calling `flake8` at the root of the repo.
 """
 import numpy as np
-from numpy.core.numeric import Inf
 
 
 def max_index(X):
@@ -40,15 +39,16 @@ def max_index(X):
     """
     i = 0
     j = 0
-    index_max = -float('Inf')
-    X_shape = X.shape
 
     # TODO
 
     if type(X) != np.ndarray:
         raise ValueError("This is not a numpy array")
-    elif len(X_shape) != 2:
+    elif len(X.shape) != 2:
         raise ValueError("The input shape is not 2D")
+
+    X_shape = X.shape
+    index_max = -float('inf')
 
     for u in range(X_shape[0]):
         for v in range(X_shape[1]):
@@ -57,6 +57,7 @@ def max_index(X):
                 i = u
                 j = v
     return i, j
+
 
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
@@ -78,11 +79,9 @@ def wallis_product(n_terms):
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
 
-    return 0.
-
     if n_terms == 0:
         return 2.
-    prod = 1
+    pi = 1
     for i in range(n_terms):
-        prod = prod*(4*(i+1)**2)/(4*(i+1)**2-1)
-    return prod*2
+        pi = pi*((4*(i+1)**2)/(4*(i+1)**2-1))
+    return pi*2
